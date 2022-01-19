d4d05487629964e5d2f1d4ef7bc91a9538d06a2a
AliHashemiS <79598781+AliHashemiS@users.noreply.github.com>
2022-01-19 14:54:44 -0600

    Created searching view

--- src/app/components/searcher/searcher.tsx
+++ src/app/components/searcher/searcher.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-
 export class Searcher extends React.Component {
 
   render() {
     return (
-      <h1>Hola desde Bdasdscador!</h1>
+      <>
+      <head>
+        <title>Bootstrap Example</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
+      </head>
+        <div className="container">
+          <h2>Tabla de Funciones</h2>
+          <input className="form-control" id="myInput" type="text" placeholder="Search.."></input>
+          <br></br>
+          <table className="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Categoría</th>
+                <th>JS</th>
+              </tr>
+            </thead>
+            <tbody id="myTable">
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </>
     );
   }
-
 }
