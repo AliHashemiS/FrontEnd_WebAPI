c6a9a48b8c9a8a016e4046101a1f8a88e5f9b86f
AliHashemiS <79598781+AliHashemiS@users.noreply.github.com>
2022-01-21 16:57:47 -0600

    Method model
    Edited

--- src/app/models/method.model.ts
+++ src/app/models/method.model.ts
@@ -2,7 +2,8 @@ export interface MethodModel{
   id?:number,
   name:string,
   id_category:number,
-  category?:string,
+  category?:{name:string},
+  user?:{email:string},
   id_user:number,
   code:string,
   description:string
